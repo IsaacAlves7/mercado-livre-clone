5d572694b45e2880043393538612223b74cc12f9
Isaac Alves <isaac.alves0720@gmail.com>
2020-08-23 20:57:51 -0300

    first commit

---
+++ src/view/Index/Main/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text, ScrollView, Image, TouchableOpacity } from 'react-native';
+
+import styles from './styles';
+
+export default function Main() {
+  return (
+  <ScrollView>
+      <Text>Main Page</Text>
+  </ScrollView>
+  );
+}
